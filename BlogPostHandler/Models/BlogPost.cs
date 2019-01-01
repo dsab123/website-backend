0f769639b1ca4daf0cffc01f296b724dd75a42fe
dsab123 <dsabbaghumd@gmail.com>
2018-12-31 21:42:13 -0500

    added DataMembers to make models come back as camelCase

--- BlogPostHandler/Models/BlogPost.cs
+++ BlogPostHandler/Models/BlogPost.cs
@@ -1,22 +1,28 @@
 ﻿
 using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 namespace BlogPostHandler.Models
 {
     public class BlogPost
     {
+        [DataMember(Name = "id", EmitDefaultValue = false)]
         public int Id { get; set; }
 
+        [DataMember(Name = "content", EmitDefaultValue = false)]
         public string Content { get; set; } // surely pulling down a huge string as a blogpost will prove poor performance; I'll move this into its own object at some point
 
+        [DataMember(Name = "blurb", EmitDefaultValue = false)]
         public bool Blurb { get; set; }
 
+        [DataMember(Name = "metadata", EmitDefaultValue = false)]
         public Metadata Metadata;
 
-        public static int BlurbLength = 50; // how many chars to show in blurbs
-
+        [DataMember(Name = "relatedPosts", EmitDefaultValue = false)]
         public List<BlogPost> RelatedPosts;
+
+        public static int BlurbLength = 50; // how many chars to show in blurbs
 
         [JsonConstructor]
         public BlogPost(int id, bool blurb = false)
